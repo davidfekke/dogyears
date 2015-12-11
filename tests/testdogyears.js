fa84553c2e381d067fdfd84b6b727fd7928e1cd4
David Fekke <david@fekke.com>
2015-12-10 22:02:42 -0500

    Adding a unit test

--- tests/testdogyears.js
+++ tests/testdogyears.js
@@ -1,3 +1,14 @@
-var DogYears = require('../lib/dogyears.js');
+var calculateDogYears = require('../lib/dogyears.js');
+var mocha = require('mocha');
+var expect = require('chai').expect;
 
-console.log(DogYears);
+var dogYears = calculateDogYears(12, 10);
+
+describe('Test DogYears module', function() {
+	it('Test result of a dog that 12 in actual years and weighs 12 pounds.', function() {
+		var result = calculateDogYears(12, 10);
+		expect(64.00810235249003).to.equal(result);
+	});
+});
+
+//console.log(dogYears);
