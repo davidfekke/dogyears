46940fa2c6adc9f0e9108ac9133e35b4667cfdf3
David Fekke <david@fekke.com>
2016-01-15 12:12:36 -0500

    Adding gulp and coveralls

--- tests/testdogyears.js
+++ tests/testdogyears.js
@@ -1,12 +1,11 @@
+'use strict';
 var calculateDogYears = require('../lib/dogyears.js');
-var mocha = require('mocha');
+//var mocha = require('mocha');
 var expect = require('chai').expect;
 
-var dogYears = calculateDogYears(12, 10);
-
-describe('Test DogYears module', function() {
-	it('Test result of a dog that 12 in actual years and weighs 12 pounds.', function() {
-		var result = calculateDogYears(12, 10);
-		expect(64.00810235249003).to.equal(result);
-	});
+describe('Test DogYears module', function () {
+    it('Test result of a dog that 12 in actual years and weighs 12 pounds.', function () {
+        var result = calculateDogYears(12, 10);
+        expect(64.00810235249003).to.equal(result);
+    });
 });
